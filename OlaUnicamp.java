6d229109291e74d4868cd2e58f355547bb577e1e
Marcelo Silva <marcelopts151@gmail.com>
2023-05-12 23:36:58 -0300

    added to src

---
+++ OlaUnicamp.java
@@ -0,0 +1,13 @@
+package br.unicamp.ic.inf335;
+
+/**
+ * Hello world!
+ *
+ */
+public class OlaUnicamp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Olá Unicamp" );
+    }
+}
